b03baf3b3ba0659b9336e5226d167dd58e6c1ee9
Mohd Bilal <billusaifi063@gmail.com>
2026-07-19 18:22:06 +0530

    Learned Basics of Operator

--- Basics of JAVA/Operators.java
+++ Basics of JAVA/Operators.java
@@ -1,5 +1,15 @@
 public class Operators {
     public static void main(String[] args) {
-        double
+        double num1 = 3.664;
+        int num2 = 4;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+
+        // diff types to modify already existing no.
+        num2 = num2 + 1;
+        System.out.println(num2); // Yahan num2 update hone ke baad 5 print karega
     }
 }
